6dfaf56a47cbb7427d264da906a50568e057df55
ElricFoong <elricfoong@gmail.com>
2023-01-23 21:46:57 +1100

    add count

--- guess num.py
+++ guess num.py
@@ -1,10 +1,13 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     num = input("guess number ")
     num = int(num)
     if num == r:
         print(" Thats right ")
+        print("this is your count ", count, "times")
         break
     elif num < r:
         
@@ -12,3 +15,4 @@ while True:
     elif num > r:
         
         print(" smaller number ")
+    print("this is your count ", count, "times")
